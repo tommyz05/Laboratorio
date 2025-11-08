7e3c68ae59c59b8c4650dbc49022a533c77669cc
tommy <tommyanima3@gmail.com>
2025-11-08 15:38:45 +0100

    Aggiunta classe activity

---
+++ Activity.h
@@ -0,0 +1,27 @@
+//
+// Created by tommy on 07/11/25.
+//
+
+#ifndef PROGETTO_ACTIVITY_H
+#define PROGETTO_ACTIVITY_H
+#include <string>
+
+class Activity
+{
+    public:
+        Activity(std::string const &descr, int const &sHour,int const &sMin, int const &eHour, int const &eMin): description(descr),startHour(sHour),startMin(sMin),endHour(eHour),endMin(eMin){}
+        void setDescription(std::string const &descr) {description = descr;}
+        std::string getDescription() const {return description;}
+        void setStartHour(int const &hour) {startHour = hour;}
+        void setStartMin(int const &min) {startMin = min;}
+        void setEndHour(int const &hour) {endHour = hour;}
+        void setEndMin(int const &min) {endMin = min;}
+        int getStartHour() const {return startHour;}
+        int getStartMin() const {return startMin;}
+        int getEndHour() const {return endHour;}
+        int getEndMin() const {return endMin;}
+    protected:
+        std::string description;
+        int startHour, startMin, endHour, endMin;
+};
+#endif //PROGETTO_ACTIVITY_H
