4167e6176818acec5f196501c64c6339e99e25e0
tommy <tommyanima3@gmail.com>
2025-12-14 19:21:11 +0100

    Aggiunta due test per altri due casi di input riguardanti il tempo delle attività

--- googletests/ActivityTest.cpp
+++ googletests/ActivityTest.cpp
@@ -22,3 +22,15 @@ TEST(ActivityTest, NegativeTimeException)
     Activity a("",-10,0,11,0);
     }, std::invalid_argument);
 }
+TEST(ActivityTest, HourOutOfBound)
+{
+    EXPECT_THROW({
+    Activity a("",24,0,24,1);
+    }, std::invalid_argument);
+}
+TEST(ActivityTest, MinutesOutOfBound)
+{
+    EXPECT_THROW({
+    Activity a("",10,64,11,65);
+    }, std::invalid_argument);
+}
